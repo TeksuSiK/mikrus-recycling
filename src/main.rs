0c05ffb560850c0d8ed6eda1604f10e0401c6d70
TeksuSiK <ntuspg@gmail.com>
2022-02-24 19:40:39 +0100

    Add discord webhook library, sending alert if server is available

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+
+use webhook::client::{WebhookClient, WebhookResult};
+
+#[tokio::main]
+async fn main() -> WebhookResult<()> {
+    let response = reqwest::get("https://mikr.us/recykling.txt")
+        .await?
+        .text()
+        .await?;
+
+    let url;
+    match env::var("discord") {
+        Ok(val) => url = val,
+        Err(err) => {
+            eprintln!("Erorr: {:?}", err);
+            process::exit(1);
+        },
+    }    
+
+    let client = WebhookClient::new(&url);
+
+    if response != "Baza jest aktualnie pusta" {
+        client.send(|message| message
+            .content("@everyone Nowy serwer dostępny w recyklingu")
+            .username("mikrus_recykling")
+            .avatar_url("https://www.wykop.pl/cdn/c3201142/comment_pMhcEOCLm7cv7OaJ6IHjqkHE72jpX2nw.jpg")).await?;
+    }
+
+    Ok(())
 }
